8ffd33ac4f6dde3f2f6007097e55b59cd53ada05
unknown <girikorat@gmail.com>
2025-10-14 16:30:27 +0530

    Change to the Comment

--- src/main/java/com/example/HelloWorldServer.java
+++ src/main/java/com/example/HelloWorldServer.java
@@ -8,7 +8,7 @@ public class HelloWorldServer {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
         server.createContext("/", exchange -> {
-            String response = "Hello World! This is running from GitHub Actions on Kubernetes.";
+            String response = "Hello World! This is running from GitHub Actions on Kubernetes.Project by Giridhar";
             exchange.sendResponseHeaders(200, response.getBytes().length);
             try (OutputStream os = exchange.getResponseBody()) {
                 os.write(response.getBytes());
